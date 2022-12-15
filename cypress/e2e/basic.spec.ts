0e18e56a0b389729ab54ac26f58d8bf502b778c3
xbmlz <997909544@qq.com>
2022-12-15 16:48:40 +0800

    ci: update e2e

--- cypress/e2e/basic.spec.ts
+++ cypress/e2e/basic.spec.ts
@@ -5,7 +5,5 @@ context('Basic', () => {
 
   it('basic nav', () => {
     cy.url().should('eq', 'http://localhost:3333/')
-
-    cy.get('#input').type('Vitesse{Enter}').url().should('eq', 'http://localhost:3333/hi/Vitesse')
   })
 })
